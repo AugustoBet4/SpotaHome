2a6067468bcb149c18396ba665385d09ef633c71
Adriana Centellas <adrianacentellas98@gmail.com>
2018-11-15 03:42:58 -0400

    Arreglo de rutas

--- SpotAHome/app/Http/Controllers/MapaEmpleadoController.php
+++ SpotAHome/app/Http/Controllers/MapaEmpleadoController.php
@@ -52,7 +52,7 @@ class MapaEmpleadoController extends Controller
     public function mapageneral(){
 
         $lonlat = DB::table('propiedad')
-            ->select('propiedad.latitud','propiedad.longitud')
+            ->select('propiedad.latitud','propiedad.longitud','propiedad.direccion')
             ->get();
         $config['center'] = '37.4419, -122.1419';
         $config['zoom'] = 'auto';
@@ -60,12 +60,15 @@ class MapaEmpleadoController extends Controller
         foreach($lonlat as $ll){
             $lat = (String) $ll->latitud;
             $lon = (String )$ll->longitud;
+            $dir = (String )$ll->direccion;
             $datos = $lat.",".$lon;
+            //$alert = 'alert("'.$dir.'")';
             $marker = array();
             $marker['position'] = $datos;
             $marker['onclick'] = 'alert("Me presionaste")';
             app('map')->add_marker($marker);
             }
+
 
         $map = app('map')->create_map();
         echo "<html><head><script type='text/javascript'>var centreGot = false;</script>".$map['js']."</head><body>".$map['html']."</body></html>";
