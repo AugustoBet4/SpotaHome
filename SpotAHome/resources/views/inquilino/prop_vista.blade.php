858782aed4fd1a19415a1cef9410411cbee31f01
danielgp <dangip96@gmail.com>
2018-11-21 12:53:58 -0400

    Modal fechas

--- SpotAHome/resources/views/inquilino/prop_vista.blade.php
+++ SpotAHome/resources/views/inquilino/prop_vista.blade.php
@@ -45,15 +45,54 @@
                         <br><br>
                         <h4>{{ $propiedad->descripcion}}</h4>
                         <h1>Bs.{{ $propiedad->costo}}</h1>
-
-
-
+                        <br><br>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Reservar</button>
                   </div>
 
 
             </div>
           </div>
         </form>
+      </div>
+      <div class="modal inmodal" id="myModal" tabindex="-1" role="dialog" aria-hidden="true">
+          <div class="modal-dialog">
+              <div class="modal-content animated bounceInRight">
+                  <div class="modal-header">
+                      <b>Verifica la disponibilidad</b>
+                      <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+
+                  </div>
+                  <div class="modal-body">
+
+
+                          <form method="POST" action="{{ action('ValoracionPropiedadController@store') }}" role="form">
+                              {{ csrf_field() }}
+
+                              <div class="form row">
+                                <div  class="col-md-12">
+                                  <input type="hidden" name="id_propiedad" class="form-control input-sm" id="id_propiedad">
+                                  <input type="hidden" name="id_inquilino" class="form-control input-sm" id="id_inquilino">
+                                  <label for="puntuacion">Fecha de Inicio: </label>
+                                  <input type="date" name="puntuacion" class="form-control input-sm" id="puntuacion"  required>
+                                  <div class="form-group">
+                                      <label for="comentario">Fecha Final: </label>
+                                      <input type="date" name="comentario" class="form-control input-sm" id="comentario"  required>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <div class="form-group col-md-6">
+                                  <input type="submit" value="Guardar" class="btn btn-success">
+                                  <button type="button" class="btn btn-white" data-dismiss="modal">Cerrar</button>
+                                  <br>
+                              </div>
+                          </form>
+                  </div>
+                  <div class="modal-footer">
+                      <br>
+                  </div>
+              </div>
+          </div>
       </div>
 
 @endsection
