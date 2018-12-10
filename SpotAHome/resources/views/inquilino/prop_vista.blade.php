4b064fe125cf7a6669578a62dab684ad967e8293
dav998 <daalfaro96@gmail.com>
2018-12-10 15:10:50 -0400

    Pruebas Comentarios 1

--- SpotAHome/resources/views/inquilino/prop_vista.blade.php
+++ SpotAHome/resources/views/inquilino/prop_vista.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'Busqueda')
 
 @section('content')
+
+    {{ Request::url()}}
     <div class="wrapper wrapper-content animated fadeInRight">
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&key=AIzaSyCCl8pKiQLLBFsI7nPksLDg-kahkBAyBtk"></script>
         <script type="text/javascript">var centreGot = false;</script>
@@ -116,4 +118,24 @@
             </div>
         </div>
     </div>
+    <div id="disqus_thread"></div>
+    <script>
+
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+
+        var disqus_config = function () {
+            this.page.url = 'http://localhost:8000/inquilino/prop_vista/1';  // Replace PAGE_URL with your page's canonical URL variable
+            this.page.identifier = '{{$propiedad->id_propiedad}}'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://spotahomesis.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 @endsection
