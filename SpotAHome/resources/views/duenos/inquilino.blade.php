157b0ff4d369a7a4343772420006232334275fdf
dav998 <daalfaro96@gmail.com>
2018-12-12 18:43:40 -0400

    Correccion identificador de comentario dueno a inquilino

--- SpotAHome/resources/views/duenos/inquilino.blade.php
+++ SpotAHome/resources/views/duenos/inquilino.blade.php
@@ -41,7 +41,7 @@
 
         var disqus_config = function () {
         this.page.url = '{{Request::url()}}';  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = {{$inquilino->id_inquilino}}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        //this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
         };
 
         (function() { // DON'T EDIT BELOW THIS LINE
@@ -52,5 +52,6 @@
         })();
     </script>
     <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
 
 @endsection
